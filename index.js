17fca5eb8416b796676086dc411dd93dee51d83e
Alexandre Bekor <contato@alexandrebekor.com>
2021-09-18 15:47:00 -0300

    Added EJS

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
+// Template
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 // Database
 const mongo = process.env.DATABASE || 'mongodb://localhost/webapp-myseries'
 mongoose.Promise = global.Promise
