e3e99637fd1646de68ca8149c13809e38b7eb27f
Alexandre Bekor <contato@alexandrebekor.com>
2021-09-18 15:35:53 -0300

    Added instance mongoose in index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// Server
 const app = express()
 const port = process.env.PORT || 3000
+
+// Database
+const mongo = process.env.DATABASE || 'mongodb://localhost/webapp-myseries'
+mongoose.Promise = global.Promise
+
+mongoose
+    .connect(mongo)
+    .then(() => {
+        app.listen(port, () => console.log('connected'))
+    })
+    .catch(e => {
+        console.log(e)
+    })
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
-app.listen(port, () => console.log('connected'))
