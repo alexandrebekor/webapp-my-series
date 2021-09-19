434455fc2eea3b590ee4a9086523a123f3462c0f
Alexandre Bekor <contato@alexandrebekor.com>
2021-09-19 13:31:46 -0300

    New layout to series

--- controllers/series.js
+++ controllers/series.js
@@ -1,5 +1,21 @@
-const home = (req, res) => res.render('series/home')
-const create = (req, res) => res.render('series/create')
+const Serie = require('../models/serie')
+
+const home = (req, res) => {
+    Serie.find({}, (err, docs) => {
+        res.render('series/home', {
+            series: docs
+        })   
+    })
+}
+
+const create = (req, res) => {
+    // const serie = new Serie({
+    //     name: 'Dark',
+    //     status: 'watching'
+    // })
+    // serie.save(() => console.log('Saved'))
+    //res.render('series/create')
+}
 
 module.exports = {
     home,
