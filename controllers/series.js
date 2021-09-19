4639152e694af22d083d06fd6a04bb975e37c3aa
Alexandre Bekor <contato@alexandrebekor.com>
2021-09-19 14:59:44 -0300

    Resource Controllers

--- controllers/series.js
+++ controllers/series.js
@@ -1,23 +1,44 @@
-const Serie = require('../models/serie')
+const Serie = require("../models/serie")
 
-const home = (req, res) => {
-    Serie.find({}, (err, docs) => {
+const home = ({ seriesModel }, req, res) => {
+    seriesModel.find({}, (err, docs) => {
         res.render('series/home', {
             series: docs
-        })   
+        })
     })
 }
 
 const create = (req, res) => {
-    // const serie = new Serie({
-    //     name: 'Dark',
-    //     status: 'watching'
-    // })
-    // serie.save(() => console.log('Saved'))
-    //res.render('series/create')
+    res.render('series/create')
+}
+
+const store = ({ seriesModel }, req, res) => {
+    const serie = new Serie(req.body)
+    serie.save(() => console.log('Record added'))
+    res.redirect('/series')
+}
+
+const show = ({ seriesModel }, req, res) => {
+
+}
+
+const edit = ({ seriesModel }, req, res) => {
+
+}
+
+const update = ({ seriesModel }, req, res) => {
+
+}
+
+const destroy = ({ seriesModel }, req, res) => {
+    seriesModel.deleteOne({ _id: req.params.id }, (err) => {
+        res.redirect('/series')
+    })
 }
 
 module.exports = {
     home,
-    create
+    create,
+    store,
+    destroy
 }
--- routes/series.js
+++ routes/series.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const seriesController = require('../controllers/series')
+const seriesModel = require('../models/serie')
 
-router.get('/', seriesController.home)
+router.get('/', seriesController.home.bind(null, { seriesModel }))
 router.get('/adicionar', seriesController.create)
+router.post('/adicionar', seriesController.store.bind(null, { seriesModel }))
+router.get('/excluir/:id', seriesController.destroy.bind(null, { seriesModel }))
 
 module.exports = router
